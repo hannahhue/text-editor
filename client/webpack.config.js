ce2ee1a00acea1eb79f2f18581251a99eaee95ec
unknown <alicia9790972@gmail.com>
2022-11-13 14:16:05 -0500

    json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,10 @@
+//define
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+//export all
 module.exports = () => {
   return {
     mode: "development",
@@ -59,7 +58,6 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: "babel-loader",
             options: {
