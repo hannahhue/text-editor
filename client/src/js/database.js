6af9271c5dca462de9e388cb717bb1a43810f034
unknown <alicia9790972@gmail.com>
2022-11-13 15:39:43 -0500

    fixed getdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,23 +1,23 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB("jate", 2, {
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      db.createObjectStore("jate");
       console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log("Post to the database");
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB("jate", 1);
+  const contentDb = await openDB("jate");
 
   // Create a new transaction and specify the database and data privileges.
   const tx = contentDb.transaction("jate", "readwrite");
@@ -26,10 +26,7 @@ export const putDb = async (id, content) => {
   const store = tx.objectStore("jate");
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({
-    id,
-    content,
-  });
+  const request = store.put(content, "Editor");
 
   // Get confirmation of the request.
   const result = await request;
@@ -41,7 +38,7 @@ export const getDb = async () => {
   console.log("GET from the database");
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB("jate", 1);
+  const contentDb = await openDB("jate");
 
   // Create a new transaction and specify the database and data privileges.
   const tx = contentDb.transaction("jate", "readonly");
@@ -50,7 +47,7 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get("Editor");
 
   // Get confirmation of the request.
   const result = await request;
