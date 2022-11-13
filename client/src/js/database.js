6cbea70b0fff26523150862c8ad6b81fb0eaab6f
unknown <alicia9790972@gmail.com>
2022-11-13 15:11:27 -0500

    get and put broken

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,13 +17,13 @@ export const putDb = async (id, content) => {
   console.log("Post to the database");
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB("content", 1);
+  const contentDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contentDb.transaction("content", "readwrite");
+  const tx = contentDb.transaction("jate", "readwrite");
 
   // Open up the desired object store.
-  const store = tx.objectStore("content");
+  const store = tx.objectStore("jate");
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({
@@ -41,13 +41,13 @@ export const getDb = async () => {
   console.log("GET from the database");
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB("content", 1);
+  const contentDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contentDb.transaction("content", "readonly");
+  const tx = contentDb.transaction("jate", "readonly");
 
   // Open up the desired object store.
-  const store = tx.objectStore("content");
+  const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
